0c5b750d68a180068462a2daae3d7789b1ecd6e1
Joshua Rolph <joshua.rolph657@gmail.com>
2025-01-03 18:10:23 +0000

    Multiplayer character loading works. Just started physics part.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import { Loader, PerformanceMonitor, SoftShadows } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { Experience } from "./components/Experience";
-import { SoftShadows } from "@react-three/drei";
+import { Physics } from "@react-three/rapier";
+import { Suspense, useState } from "react";
 
 function App() {
   return (
     <Canvas shadows camera={{ position: [0, 30, 0], fov: 30 }}>
       <color attach="background" args={["#242424"]} />
       <SoftShadows size={42} />
-      <Experience />
+        <Suspense>
+          <Physics>
+            <Experience downgradedPerformance={downgradedPerformance} />
+          </Physics>
+        </Suspense>
     </Canvas>
   );
 }
